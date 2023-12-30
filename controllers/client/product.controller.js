5a640cb28dbf14da822b7d539968ca311751fee7
LeQuocNam2004 <149886562+LeQuocNam2004@users.noreply.github.com>
2023-12-30 11:35:59 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,22 @@
+const Product = require('../../models/product.model');
+
 //[GET] /products/
-module.exports.index = (req, res) => {
+module.exports.index = async(req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    for (const item of products) {
+        item.priceNew = item.price * (1 - item.discountPercentage/100);
+        item.priceNew = item.priceNew.toFixed(0);
+    }
+
+    console.log(products);
+
     res.render("client/pages/products/index",{
-        pageTitle:"Trang danh sách sản phẩm"
+        pageTitle:"Trang danh sách sản phẩm",
+        products: products
     });
 }
 
