e773f92eb7c36d9a7f376e6a0635e479438b8f0c
LeQuocNam2004 <149886562+LeQuocNam2004@users.noreply.github.com>
2024-01-15 22:40:34 +0700

    Xây dựng phần  quản lý danh mục  sản phẩm(Phân cấp tree)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,6 +1,7 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require('../../config/system');
 
+const createTreeHelper = require('../../helpers/create-tree.helper');
 //[GET] /admin/products-category
 module.exports.index = async(req, res) => {
     const records = await ProductCategory.find({
@@ -15,8 +16,15 @@ module.exports.index = async(req, res) => {
 
 //[GET] /admin/products-category/create
 module.exports.create = async(req, res) => {
+    const records = await ProductCategory.find({
+        deleted: false
+    });
+
+    const newRecords = createTreeHelper(records);
+
     res.render("admin/pages/products-category/create",{
         pageTitle:"Thêm mới danh mục sản phẩm",
+        records: newRecords
     });
 };
 
