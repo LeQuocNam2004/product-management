5c5ade7879270e841dc527334377bd9e11204756
LeQuocNam2004 <149886562+LeQuocNam2004@users.noreply.github.com>
2024-01-21 22:34:49 +0700

    Xây dựng phần Phân quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -52,3 +52,37 @@ module.exports.editPatch = async (req, res) => {
 
   res.redirect(`back`);
 }
+
+// [GET] /admin/roles/permissions
+module.exports.permissions = async (req, res) => {
+  const records = await Role.find({
+    deleted: false
+  });
+
+  console.log(records);
+
+  res.render("admin/pages/roles/permissions", {
+    pageTitle: "Phân quyền",
+    records: records
+  });
+}
+
+// [PATCH] /admin/roles/permissions
+module.exports.permissionsPatch = async (req, res) => {
+  const roles = JSON.parse(req.body.roles);
+
+  try {
+    for (const item of roles) {
+      await Role.updateOne({
+        _id: item.id
+      }, {
+        permissions: item.permissions
+      });
+    }
+    req.flash("success", "Cập nhật phân quyền thành công!");
+  } catch (error) {
+    req.flash("success", "Cập nhật phân quyền không thành công!");
+  }
+
+  res.redirect("back");
+}
