e3fd32a8a6d6bf457314ffffc47313c48baf5e6f
LeQuocNam2004 <149886562+LeQuocNam2004@users.noreply.github.com>
2024-01-26 20:08:48 +0700

    Làm trang Thông tin cá nhân bên admin (Chỉnh sửa thông tin cá nhân)

--- routes/admin/my-account.route.js
+++ routes/admin/my-account.route.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+const upload = multer();
 
 const controller = require("../../controllers/admin/my-account.controller");
 
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
 router.get("/", controller.index);
+
+router.patch(
+    "/edit",
+    upload.single("avatar"),
+    uploadCloud.uploadSingle,
+    controller.editPatch
+);
 
 module.exports = router;
